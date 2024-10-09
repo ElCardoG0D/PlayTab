ac828282cc500e451cdb04e9c25977098d5e953e
KevinVivanco_02 <kev.vivanco@duocuc.cl>
2024-10-09 16:46:24 -0300

    Corrigió validaciones
    Se reintegraron las validaciones del registro.

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular'; // Importa AlertController
 
 @Component({
   selector: 'app-register',
@@ -19,20 +20,72 @@ export class RegisterPage {
 
   constructor(
     private firebaseService: FirebaseService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private alertController: AlertController
   ) {}
+
+  // Método que permite mostrar una alerta.
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
   // Método para el registro de usuario
   async SingUp() {
-    // Validación de contraseñas
-    if (this.password !== this.ConfirmPassword) {
-      alert("Las contraseñas no coinciden");
+    // Validar si todos los campos están llenos
+    if (!this.nombre || !this.rut || !this.mailuser || !this.celular || !this.password || !this.ConfirmPassword) {
+      this.presentAlert('Faltan rellenar campos');
       return;
     }
 
-    // Validación de términos y condiciones
+    // Validar Nombre
+    const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{1,30}$/;
+    if (!nameRegex.test(this.nombre)) {
+      this.presentAlert('Ingrese su nombre correctamente.');
+      return;
+    }
+
+    // Validar el rut
+    const rutRegex = /^\d{7,8}-[kK\d]$/;
+    if (!rutRegex.test(this.rut)) {
+      this.presentAlert('Ingrese un rut válido.');
+      return;
+    }
+
+    // Validar el formato del correo
+    const emailRegex = /^[^\s@]+@[^\s@]+\.(com|cl)$/i;
+    if (!emailRegex.test(this.mailuser)) {
+      this.presentAlert('El correo es inválido.');
+      return;
+    }
+
+    // Validar el formato del número de celular
+    const phoneRegex = /^\+569\d{8}$/;
+    if (!phoneRegex.test(this.celular)) {
+      this.presentAlert('Número de celular inválido.');
+      return;
+    }
+
+    // Validar la contraseña
+    if (this.password.length < 4 || this.password.length > 8) {
+      this.presentAlert('La contraseña debe tener mínimo 8 carácteres y máximo 8.');
+      return;
+    }
+
+    // Validar que ambas contraseñas sean iguales
+    if (this.password !== this.ConfirmPassword) {
+      this.presentAlert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    // Validar si los términos y condiciones ha sido aceptado
     if (!this.AceptaCondiciones) {
-      alert("Debes aceptar los términos y condiciones");
+      this.presentAlert('Debe aceptar los términos y condiciones.');
       return;
     }
 
