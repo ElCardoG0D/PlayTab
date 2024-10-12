c59001c1ee8de586fd3f030e9373e50e466c6427
KevinVivanco_02 <kev.vivanco@duocuc.cl>
2024-10-12 01:09:08 -0300

    Arreglo temporal Regiater
    Tuve que sacar el calendario y poner un input que por último limite los meses y los días en 12 y 31 max.

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -15,8 +15,10 @@ export class RegisterPage {
   celular: string = '';
   password: string = '';
   ConfirmPassword: string = '';
+  regionId: number = 0;
   comunaId: number = 0;
   fechaNacimiento: string = ''; // Agregado para la fecha de nacimiento
+  showCalendar: boolean = false;
   AceptaCondiciones: boolean = false;
   showPassword: boolean = false;
 
@@ -32,9 +34,9 @@ export class RegisterPage {
   }
 
   // Método que permite mostrar una alerta
-  async presentAlert(message: string) {
+  async presentAlert(header: string, message: string) {
     const alert = await this.alertController.create({
-      header: 'Error',
+      header: header,
       message: message,
       buttons: ['OK']
     });
@@ -45,75 +47,86 @@ export class RegisterPage {
   async SingUp() {
     // Validar si todos los campos están llenos
     if (!this.nombre || !this.rut || !this.mailuser || !this.celular || !this.password || !this.ConfirmPassword || !this.fechaNacimiento) {
-      this.presentAlert('Faltan rellenar campos');
+      this.presentAlert('Error','Faltan rellenar campos.');
       return;
     }
 
     // Validar Nombre
     const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{1,50}$/;
     if (!nameRegex.test(this.nombre)) {
-      this.presentAlert('Ingrese su nombre correctamente.');
+      this.presentAlert('Error', 'Ingrese su nombre correctamente.');
       return;
     }
 
     // Validar el rut
     const rutRegex = /^\d{7,8}-[kK\d]$/;
     if (!rutRegex.test(this.rut)) {
-      this.presentAlert('Ingrese un RUT válido.');
+      this.presentAlert('Error','Ingrese un RUT válido.');
       return;
     }
 
     // Validar el formato del correo
     const emailRegex = /^[^\s@]+@[^\s@]+\.(com|cl)$/i;
     if (!emailRegex.test(this.mailuser)) {
-      this.presentAlert('El correo es inválido.');
+      this.presentAlert('Error','El correo es inválido.');
       return;
     }
 
     // Validar el formato del número de celular
     const phoneRegex = /^\+569\d{8}$/;
     if (!phoneRegex.test(this.celular)) {
-      this.presentAlert('Número de celular inválido.');
+      this.presentAlert('Error','Número de celular inválido.');
       return;
     }
 
     // Validar la contraseña
     if (this.password.length < 4 || this.password.length > 8) {
-      this.presentAlert('La contraseña debe tener mínimo 4 caracteres y máximo 8.');
+      this.presentAlert('Error','La contraseña debe tener mínimo 4 caracteres y máximo 8.');
       return;
     }
 
     // Validar que ambas contraseñas sean iguales
     if (this.password !== this.ConfirmPassword) {
-      this.presentAlert('Las contraseñas no coinciden.');
+      this.presentAlert('Error', 'Las contraseñas no coinciden.');
+      return;
+    }
+
+    // Validar comuna
+    if (!this.regionId) {
+      this.presentAlert('Error','Debe seleccionar una región.');
       return;
     }
 
     // Validar comuna
     if (!this.comunaId) {
-      this.presentAlert('Debe seleccionar su comuna.');
+      this.presentAlert('Error','Debe seleccionar una comuna.');
       return;
+    }
+
+    const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/; // Regex para formato YYYY-MM-DD
+    if (!this.fechaNacimiento.match(regex)) {
+      this.presentAlert('Error', 'Por favor, Ingrese una fecha valida.');
     }
 
     // Validar fecha de nacimiento
     if (!this.fechaNacimiento) {
-      this.presentAlert('Debe ingresar su fecha de nacimiento.');
+      this.presentAlert('Error','Debe ingresar su fecha de nacimiento.');
       return;
     }
 
     // Validar si los términos y condiciones han sido aceptados
     if (!this.AceptaCondiciones) {
-      this.presentAlert('Debe aceptar los términos y condiciones.');
+      this.presentAlert('Error','Debe aceptar los términos y condiciones.');
       return;
     }
 
     try {
       // Llamada al servicio para registrar al usuario
       await this.dbService.registerUser(this.rut, this.nombre, this.mailuser, this.password, this.comunaId, this.fechaNacimiento).toPromise();
-      this.presentAlert('Usuario registrado con éxito.');
+      this.presentAlert('¡Felicidades!','Usuario registrado con éxito.');
       this.router.navigate(['./login']);
     } catch (error) {
-      this.presentAlert('Error al registrar el usuario.');
+      this.presentAlert('Error', 'No se pudo registrar el usuario.');
     }
   }
 }
